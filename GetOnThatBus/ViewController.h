fddf037d81de9501040f5f98f339743333c664bc
benbueltmann <benbueltmann@gmail.com>
2014-08-05 13:13:36 -0500

    Initial Commit

---
+++ GetOnThatBus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GetOnThatBus
+//
+//  Created by Ben Bueltmann on 8/5/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
